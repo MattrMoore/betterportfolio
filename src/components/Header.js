bf326e453992d6f91562f6e20bfebeb0cd179245
Matt Moore <mattmoore2014@gmail.com>
2023-05-31 14:38:31 -0600

    ,

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,23 @@
 import React from "react";
 
 const styles = {
-    header: {
-        background: "#7DD0FF",
-        height: "100px",
-        top: "0",
-        left: "0",
-        width: "100%",
-    },
+    
     h1: {
         fontSize: "100px",
         color: "#3cd6f9",
-        textAlign: "left",
+        textAlign: "center",
         margin: "0",
         paddingLeft: "20px",
         paddingTop: "30px",
-        textShadow: "1px 1px 2px red, 0 0 0.2em blue, 0 0 0.2em blue",
+        textShadow: "1px 1px 2px pink, 0 0 0.2em blue, 0 0 0.2em purple",
     }
 };
 
 function Header() {
     return(
-    <header className="col-1" style={styles.header}>
-        <h1 style={styles.h1}>Matt</h1>
-    </header>
+    <div class="header" className="col-1" style={styles.header}>
+        <h1 style={styles.h1}>Matt's Portfolio</h1> 
+        </div> 
     );
 }
 
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,9 +3,9 @@ import me from '../images/me.jpg';
 
 export default function About() {
   return (
-    <div style={{ marginTop: 30 }}>
+    <div style={{ marginTop: 45 }}>
       <img src={me} alt="me" />
-      <h1>About Page</h1>
+      <h1>About Me!</h1>
       <p>
         I'm a full stack web developer with a background in customer service and customer service management. I learned coding through the University of Denver Bootcamp program and am working on getting my Associates in Computer Science at Community College of Denver. I'm currently living in the Denver Metro area, and I am looking for an Entry level Programming Job either in this area or in Rhode Island. I work hard and like to challenge myself everyday and will make a valuable asset for your team after the intitial learning curve, I love learning and discovering new systems, technologies and languages to use.
       </p>
